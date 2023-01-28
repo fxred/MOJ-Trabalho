5f24ef109a6755401a53c626e47ec975b7a228ac
fxred <fxr.ed03@gmail.com>
2023-01-28 20:38:34 -0300

    Adicionados métodos de inicializar stack e inserir no começo, falta consertar a main pra rodar dentro de um while

--- C-z.c
+++ C-z.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct node {
+    char value[100];
+    struct node *next;
+} node;
+
+void initializeStack(node *head) {
+    strcpy(head->value, "NULL");
+    head->next = NULL;
+}
+
+void insertAtBeginning(node *head, char inputNoKeyword[]) { // método PUSH, provavelmente vai precisar de refatoração
+    if (!(strcmp(head->value, "NULL"))) {
+        strcpy(head->value, inputNoKeyword);
+    }
+    else {
+        node *temp = head;
+        strcpy(head->value, inputNoKeyword);
+        head->next = temp;
+    }
+}
+
+// void removeAtBeginning(node *head) { // método POP
+//     if (head->value == "NULL") {
+//         head->value = inputNoKeyword;
+//     }
+//     else {
+
+//     }
+// }
 
 int main() {
+
+    node *head = malloc(sizeof(node));
+
+    initializeStack(head);
+
     char input[100];
 
     scanf("%[^\n]", input);
@@ -12,9 +48,11 @@ int main() {
     }
     else {
         char inputNoKeyword[strlen(input)-8];
-        for (int i = 8; i < strlen(input)+1; i++) { // +1 é por causa do '\0'
+        for (unsigned int i = 8; i < strlen(input)+1; i++) { // +1 é por causa do '\0'
             inputNoKeyword[i-8] = input[i];
         }
         // TODO: implementar o método push da pilha
+        insertAtBeginning(head, inputNoKeyword);
     }
+    printf("%s", head->value);
 }
