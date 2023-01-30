087be40893827c7ff994007b8173d1ba6a3f2717
Daniel Sousa <sousadanielsb@gmail.com>
2023-01-29 21:53:39 -0300

    Premissa Inicial para a questão 5

---
+++ 5-Turne.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    /*
+        1 - Criar Fila encadeada
+        1.1 - Fazer N interações para descobrir o tamanho da FIla
+        
+        2 - Criar dois vetores com esse valor.
+        2.1 - Salvar o primeiro valor da fila
+        2.2 - Desenfilar
+
+        3 - Verificar entre os dois proximos valores qual vai para o vetor
+        3.1 - Salvar no vetor o valor
+        3.2 - Desemfilar e salvar no outro vetor
+        3.3 - Terminar de desenfilar e salvar nesse Vetor
+        3.4 - Re-enfilar o vetor.
+
+        4 - Repetir passo 3 até não ter mais valores na fila
+    */
+}
