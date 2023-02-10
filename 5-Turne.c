42370f6bfb2c855416d3a73d942201e575127d49
Eduardo <fxr.ed03@gmail.com>
2023-02-09 23:09:21 -0300

    adicionado 5-turne.c

--- 5-Turne.c
+++ 5-Turne.c
@@ -1,20 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    char value[27];
+    struct node *next;
+} node;
+
+node *head;
+node *tail;
+
+int queueInitialize(node *n) {
+    if (n == NULL) {
+        return 0;
+    }
+    node* head = NULL;
+    node* tail = NULL;
+    return 1;
+}
+
+char * check(node *n) {
+    return head->value;
+}
+
+void enqueue(node *n, char valueToEnqueue[]) {
+    node *temp = malloc(sizeof(node));
+    strcpy(temp->value, valueToEnqueue);
+    temp->next = NULL;
+
+    if (head == NULL && tail == NULL) {
+        head = temp;
+        tail = temp;
+    }
+    else {
+        tail->next = temp;
+        tail = temp;
+    }
+}
+
+void dequeue(node *n) {
+    if (head != NULL && head != tail) {
+        node *temp = head;
+        char *value = temp->value;
+        head = temp->next;
+        free(temp);
+    }
+    else if (head == tail && head != NULL && tail != NULL) {
+        node *temp = head;
+        char *data = head->value;
+        head = NULL;
+        tail = NULL;
+        free(temp);
+    }
+    else {
+        printf("You cannot remove a node from an empty list.\n");
+    }
+}
+
+int isEmpty(node *n) {
+    return head == NULL && tail == NULL;
+}
 
 int main(){
-    /*
-        1 - Criar Fila encadeada
-        1.1 - Fazer N interações para descobrir o tamanho da FIla
-        
-        2 - Criar dois vetores com esse valor.
-        2.1 - Salvar o primeiro valor da fila
-        2.2 - Desenfilar
+   char leitura[27];
 
-        3 - Verificar entre os dois proximos valores qual vai para o vetor
-        3.1 - Salvar no vetor o valor
-        3.2 - Desemfilar e salvar no outro vetor
-        3.3 - Terminar de desenfilar e salvar nesse Vetor
-        3.4 - Re-enfilar o vetor.
+   node *n = malloc(sizeof(node));
+   queueInitialize(n);
+   
+   while (scanf("%s", leitura) == 1) {
+    enqueue(n, leitura);
+   }
 
-        4 - Repetir passo 3 até não ter mais valores na fila
-    */
+   while (!isEmpty(n)) {
+    node *temp;
+    strcpy(temp->value, check(n));
+    temp->next = NULL;
+    
+    printf("%s\n", temp->value);
+    dequeue(n);
+    if (isEmpty(n)) {
+        break;
+    }
+
+    if (temp->value[strlen(temp->value) - 1] == check(n)[0]+32) {
+        enqueue(n, check(n));
+        dequeue(n);
+    }
+   }
 }
