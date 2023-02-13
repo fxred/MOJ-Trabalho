868d8c281c2a6713b21f60984ad6099526fd6ea2
fxred <fxr.ed03@gmail.com>
2023-02-13 08:51:03 -0300

    adicionado quicksort mais eficiente: insertion+mediana de 3 (ainda TLE 81p)

--- 10-SanidadeVetorQuick.c
+++ 10-SanidadeVetorQuick.c
@@ -17,36 +17,88 @@ typedef node Item;
 #define cmpexch(A, B) {if(less(B, A)) exch(A, B)}
 
 //Versão Otimizada
-int Separa(Item *n, int l, int r){
-    Item pivo = n[r];
+// int Separa(Item *n, int l, int r){
+//     Item pivo = n[r];
     
-    int j = l;
+//     int j = l;
 
-    for(int i = l; i < r; i++){
-        if(lessequal(n[i], pivo)){
-            exch(n[i], n[j]);
-            j++;
-        }
+//     for(int i = l; i < r; i++){
+//         if(lessequal(n[i], pivo)){
+//             exch(n[i], n[j]);
+//             j++;
+//         }
+//     }
+
+//     exch(n[j], n[r]);
+    
+//     return j;
+// }
+
+int separa(Item *V,int l,int r)
+{
+  Item c=V[r];
+  int j=l;
+  for(int k=l;k<r;k++)
+    if(less(V[k],c))
+    {
+      exch(V[k],V[j]);
+      j++;
     }
+  exch(V[j],V[r]);
+  return j;
+}
 
-    exch(n[j], n[r]);
-    
-    return j;
+void insertionsort(Item *v,int l,int r)
+{
+  for(int i=r;i>l;i--)
+    cmpexch(v[i-1],v[i]);
+
+  for(int i=l+2;i<=r;i++)
+  {
+    int j=i; Item tmp=v[j];
+    while(less(tmp,v[j-1]))
+    {
+      v[j]=v[j-1];
+      j--;
+    }
+    v[j]=tmp;
+  }
+}
+
+static void quicksortM3(Item *V,int l, int r)
+{
+  if (r-l<=32) return;
+
+  exch(V[(l+r)/2],V[r-1]);
+  cmpexch(V[l],V[r-1]);
+  cmpexch(V[l],V[r]);
+  cmpexch(V[r-1],V[r]);
+
+
+  int j=separa(V,l+1,r-1);
+  quicksortM3(V,l,j-1);
+  quicksortM3(V,j+1,r);
+}
+
+void quicksortM3insertion(Item *V,int l,int r)
+{
+  quicksortM3(V,l,r);
+  insertionsort(V,l,r);
 }
 
 
 
-void quicksort(node *n, int l, int r){
-    if(r <= l){return;}
+// void quicksort(node *n, int l, int r){
+//     if(r <= l){return;}
 
-    cmpexch(n[((l + r)/ 2)], n[r]);
-    cmpexch(n[l], n[((l + r)/ 2)]);
-    cmpexch(n[r], n[((l + r)/ 2)]);
+//     cmpexch(n[((l + r)/ 2)], n[r]);
+//     cmpexch(n[l], n[((l + r)/ 2)]);
+//     cmpexch(n[r], n[((l + r)/ 2)]);
     
-    int j = Separa(n, l, r);
-    quicksort(n, l, (j - 1));
-    quicksort(n, (j + 1), r);
-}
+//     int j = Separa(n, l, r);
+//     quicksort(n, l, (j - 1));
+//     quicksort(n, (j + 1), r);
+// }
 
 
 int binarySearch(node *n, int l, int r, long unsigned int find) {
@@ -115,7 +167,7 @@ int main() {
         n[i++].post = post;
     }
 
-    quicksort(n, 0, i-1);
+    quicksortM3insertion(n, 0, i-1);
 
     // for (int j = 0; j < i; j++) {
     //     printf("%lx %lx %lx\n", n[j].curr, n[j].prev, n[j].post);
